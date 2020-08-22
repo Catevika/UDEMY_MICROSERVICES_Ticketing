b76701d6d1870ed7fc1b351f25a2d6b0f2e9da21
catevika <dominique.bello@outlook.fr>
2020-08-22 18:13:20 +0200

    Solve cors issue

--- orders/src/app.ts
+++ orders/src/app.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import 'express-async-errors';
+import cors from 'cors';
 import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
 import { errorHandler, NotFoundError, currentUser } from '@catevikatickets/common';
@@ -10,6 +11,7 @@ import { showOrderRouter } from './routes/show';
 
 const app = express();
 app.set('trust proxy', true);
+app.use(cors());
 app.use(json());
 app.use(
   cookieSession({
