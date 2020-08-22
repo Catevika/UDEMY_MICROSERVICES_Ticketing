bf341fc14dde64b2596184e5b02342ddbf6a1ca0
catevika <dominique.bello@outlook.fr>
2020-08-22 18:22:08 +0200

    Back to cors unsolved for Stripe module

--- orders/src/app.ts
+++ orders/src/app.ts
@@ -1,6 +1,5 @@
 import express from 'express';
 import 'express-async-errors';
-import cors from 'cors';
 import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
 import { errorHandler, NotFoundError, currentUser } from '@catevikatickets/common';
@@ -11,7 +10,6 @@ import { showOrderRouter } from './routes/show';
 
 const app = express();
 app.set('trust proxy', true);
-app.use(cors());
 app.use(json());
 app.use(
   cookieSession({
--- payments/src/app.ts
+++ payments/src/app.ts
@@ -1,6 +1,5 @@
 import express from 'express';
 import 'express-async-errors';
-import cors from 'cors';
 import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
 import { errorHandler, NotFoundError, currentUser } from '@catevikatickets/common';
@@ -8,7 +7,6 @@ import { createChargeRouter } from './routes/new';
 
 const app = express();
 app.set('trust proxy', true);
-app.use(cors());
 app.use(json());
 app.use(
   cookieSession({
